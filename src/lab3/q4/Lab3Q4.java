999c356795bb32ddd40ae83bf074a2c712e27078
vaibhavdsgit <sawantvaibhavd@gmail.com>
2021-11-11 17:57:15 +0530

    com
    comm

--- src/lab3/q4/Lab3Q4.java
+++ src/lab3/q4/Lab3Q4.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Lab3Q4 {
 
 	public static int modifyNumber(int num) {
+		String string = Integer.toString(num);
+		StringBuffer sb = new StringBuffer();
 		
-		while(num!=0) {
-			
-		}
+		
+		return 0;
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
---
+++ src/lab3/q5/Lab3Q5.java
@@ -0,0 +1,8 @@
+package lab3.q5;
+
+
+
+public class Lab3Q5 {
+
+	
+}
