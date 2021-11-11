00416ae73ff8ee3340c0ad14a3e82bf5d7fb8491
vaibhavdsgit <sawantvaibhavd@gmail.com>
2021-11-11 17:27:00 +0530

    Create Lab3Q4.java

---
+++ src/lab3/q4/Lab3Q4.java
@@ -0,0 +1,20 @@
+package lab3.q4;
+
+import java.util.Scanner;
+
+public class Lab3Q4 {
+
+	public static int modifyNumber(int num) {
+		
+		while(num!=0) {
+			
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number:");
+		int num = sc.nextInt();
+		System.out.println(modifyNumber(num));
+	}
+
+}
