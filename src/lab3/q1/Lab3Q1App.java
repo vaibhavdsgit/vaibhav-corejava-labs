4e05ea521389f92abe731458f30365f33c2753fb
vaibhavdsgit <sawantvaibhavd@gmail.com>
2021-11-11 16:57:59 +0530

    com
    Comm

--- src/lab3/q1/Lab3Q1App.java
+++ src/lab3/q1/Lab3Q1App.java
@@ -1,24 +1,25 @@
 package lab3.q1;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Lab3Q1App {
 	
-	public static StringBuffer getImage(String string) {
-		StringBuffer revString = new StringBuffer(string);
-		return revString.reverse();
-		//System.out.println(revString);
-		
-	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String string;
-		System.out.println("Enter the String: ");	
-		string = sc.nextLine();		//EARTH
-		
-		System.out.println(string+"|"+getImage(string));	//EARTH|HTRAE
-		
-		
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in); 
+        int temp,sum=0;
+        
+        System.out.println("Enter series of numbers:");
+        String str=sc.nextLine();
+        sc.close();
+        StringTokenizer ref=new StringTokenizer(str," ");
+        while(ref.hasMoreTokens())
+        {
+        	temp=Integer.parseInt(ref.nextToken());
+        	System.out.println(temp);
+        	sum+= temp;        	
+        }
+        System.out.println("sum: "+sum);
 	}
 }
---
+++ src/lab3/q2/Lab3Q2.java
@@ -0,0 +1,22 @@
+package lab3.q2;
+
+import java.util.Scanner;
+
+public class Lab3Q2 {
+
+	public static StringBuffer getImage(String string) {
+		StringBuffer revString = new StringBuffer(string);
+		return revString.reverse();
+		//System.out.println(revString);
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String string;
+		System.out.println("Enter the String: ");	
+		string = sc.nextLine();		//EARTH
+		
+		System.out.println(string+"|"+getImage(string));	//EARTH|HTRAE
+	}
+}
